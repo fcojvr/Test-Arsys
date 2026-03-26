ec649004402f9415572d87d576cf3c1136904a74
Francisco Javier Páez Álvarez (Proyectos y Producto SDi. Digital Group) <fpaez@sdi.es>
2026-03-26 23:49:42 +0100

    cambios finales

--- jenkins/init.groovy.d/create-jobs.groovy
+++ jenkins/init.groovy.d/create-jobs.groovy
@@ -1,28 +1,19 @@
 import jenkins.model.Jenkins
 import hudson.model.*
-import javaposse.jobdsl.plugin.*
+import javaposse.jobdsl.plugin.ExecuteDslScripts
 import javaposse.jobdsl.dsl.*
 import net.sf.json.JSONObject
 
 System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
 
-/*def jobDslScript = '''
-pipelineJob("smoke-tests") {
-    displayName("Playwright Smoke Tests")
-    description("Runs Playwright smoke tests against well-known public URLs. Artifacts: HTML report + traces/screenshots.")
-
-    properties {
-        disableConcurrentBuilds()
-    }
-
-    // Configuración para que el Job clone tu repositorio automáticamente
+def jobDslCode = '''
+pipelineJob('smoke-tests') {
+    displayName('🔥 Playwright Smoke Tests')
     definition {
         cpsScm {
             scm {
                 git {
-                    remote {
-                        url('https://github.com/fcojvr/Test-Arsys.git')
-                    }
+                    remote { url('https://github.com/fcojvr/Test-Arsys.git') }
                     branch('main')
                 }
             }
@@ -32,18 +23,9 @@ pipelineJob("smoke-tests") {
 }
 '''
 
-// Seed the job using the Job DSL executor
-Thread.start {
-    sleep 5000  // Esperar a que Jenkins termine de arrancar
+// 3. Ejecución forzada inmediata
+def jobManagement = new javaposse.jobdsl.plugin.JenkinsJobManagement(System.out, [:], new File('.'))
+def dslScriptLoader = new javaposse.jobdsl.dsl.DslScriptLoader(jobManagement)
+dslScriptLoader.runScript(jobDslCode)
 
-    def jenkins = Jenkins.get()
-    def jobDslFacade = new DslScriptLoader(new JenkinsJobManagement(System.out, [:], new File(".")))
-
-    try {
-        jobDslFacade.runScript(jobDslScript)
-        println "[init.groovy] ✅ smoke-tests job created/updated via Job DSL"
-    } catch (Exception e) {
-        println "[init.groovy] ❌ Failed to seed job: ${e.message}"
-        e.printStackTrace()
-    }
-}
+println "[init.groovy] ✅ TODO LISTO: CSP desactivado y Job 'smoke-tests' creado."
